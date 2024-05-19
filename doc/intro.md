d82ab88117c1dd61a4bcd8e5e7c0c7451f0449cb
cindy1404hsieh <chsieh@fi.uba.ar>
2024-05-19 14:09:56 -0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tp_sistemas_l
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tp_sistemas_l/core_test.clj
@@ -0,0 +1,7 @@
+(ns tp-sistemas-l.core-test
+  (:require [clojure.test :refer :all]
+            [tp-sistemas-l.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
